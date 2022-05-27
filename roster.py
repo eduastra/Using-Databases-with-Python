3dab1a98b0b3ad51f0e7d511a5297c7a8f4ba728
Eduastra <cadu_gold@hotmail.com>
2022-05-27 20:29:23 -0300

    DataBase created

--- roster.py
+++ roster.py
@@ -1,3 +1,31 @@
+from fileinput import filename
 import sqlite3
 import json
-import sqlite3
+
+dbname = "roster.sqlite"
+conn =sqlite3.connect(dbname)
+cur = conn.cursor()
+
+cur.executescript('''
+   DROP TABLE IF EXISTS User;
+   DROP TABLE IF EXISTS Course;
+   DROP TABLE IF EXISTS Member;
+
+   CREATE TABLE User (
+       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+       name TEXT UNIQUE
+   );
+
+   CREATE TABLE Course (
+       id iNTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+       title TEXT UNIQUE
+   );
+
+   CREATE TABLE Member (
+       user_id INTEGER,
+       Role INTEGER,
+       PRIMARY KEY(user_id, course_id)
+   )
+''')
+
+
